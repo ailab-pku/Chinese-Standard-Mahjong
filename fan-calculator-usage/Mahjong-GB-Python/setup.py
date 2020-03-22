cd3659279701f7f2e99955ca52e1efda5dc20c15
周昊宇 <zhy.zbklqx@Gmail.com>
2020-03-22 15:08:38 +0800

    update python setup.py for smooth compile

--- fan-calculator-usage/Mahjong-GB-Python/setup.py
+++ fan-calculator-usage/Mahjong-GB-Python/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, Extension
 
 module = Extension('MahjongGB', sources=[
     'mahjong.cpp',
-    ], language='c++')
+    ], language='c++', extra_compile_args = ["-std=c++11"])
 
 setup(name='MahjongGB', ext_modules = [module])
